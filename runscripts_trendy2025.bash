59a08473dea71bedd439bcd8da80716eaf9ef510
Charles D Koven <cdkoven@lbl.gov>
2025-07-10 16:06:00 -0700

    added post-AD spinup case

--- runscripts_trendy2025.bash
+++ runscripts_trendy2025.bash
@@ -6,15 +6,15 @@ GITHASH1=`git log -n 1 --format=%h`
 cd external_models/fates
 GITHASH2=`git log -n 1 --format=%h`
 
-STAGE=AD_SPINUP
-#STAGE=POSTAD_SPINUP
+#STAGE=AD_SPINUP
+STAGE=POSTAD_SPINUP
 #STAGE=TRANSIENT_LU_CONSTANT_CO2_CLIMATE
 #STAGE=TRANSIENT_LU_TRANSIENT_CO2_CLIMATE
 
 if [ "$STAGE" = "AD_SPINUP" ]; then
-    SETUP_CASE=f19_0003_adspinup_const1700LUH_swdif
+    SETUP_CASE=f19_0005_adspinup_const1700LUH_swdif_2pftspasturerangeland
 elif [ "$STAGE" = "POSTAD_SPINUP" ]; then
-    SETUP_CASE=f19_0031_postadspinup_const1850LUH3
+    SETUP_CASE=f19_0006_postadspinup_const1700LUH
 elif [ "$STAGE" = "TRANSIENT_LU_CONSTANT_CO2_CLIMATE" ]; then
     SETUP_CASE=f19_0018_translanduse_fromconst1850lu
 fi
@@ -33,6 +33,14 @@ project=m2467
 cd $CASE_NAME
 
 ncgen -o fates_params_default_${GITHASH2}.nc ${SRCDIR}/external_models/fates/parameter_files/fates_params_default.cdl
+
+# reduce lifespan of grasses
+#/global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --pft 13 --var fates_mort_bmort --val 0.2 --O
+#/global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --pft 14 --var fates_mort_bmort --val 0.2 --O
+
+# Add additional 2 patches and pfts each for the pasture and rangeland
+/global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --O --var=fates_max_nocomp_pfts_by_landuse --val=4,4,2,2,1
+/global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --O --var=fates_maxpatches_by_landuse --val=9,5,2,2,1
 
 # add more age bins to history output
 /global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --O --var=fates_history_ageclass_bin_edges --val=0,1,2,5,10,20,50,100,200 --changeshape
@@ -57,11 +65,15 @@ if [ "$STAGE" = "AD_SPINUP"  ]; then
     ./xmlchange ELM_ACCELERATED_SPINUP=on
     ./xmlchange -id ELM_BLDNML_OPTS -val "-bgc fates -no-megan -no-drydep -bgc_spinup on"
     ./xmlchange NTASKS=-8
-    ./xmlchange STOP_N=30
+    ./xmlchange STOP_N=50
     ./xmlchange REST_N=5
     ./xmlchange STOP_OPTION=nyears
+
     ./xmlchange JOB_QUEUE=debug
     ./xmlchange JOB_WALLCLOCK_TIME=00:30:00
+
+    # ./xmlchange JOB_QUEUE=regular
+    # ./xmlchange JOB_WALLCLOCK_TIME=20:00:00
     
     #./xmlchange DATM_MODE=CLMCRUNCEP
 
@@ -71,7 +83,8 @@ if [ "$STAGE" = "AD_SPINUP"  ]; then
     ./xmlchange DATM_PRESAERO=clim_1850
 
     #./xmlchange DIN_LOC_ROOT=/dvs_ro/cfs/cdirs/e3sm/inputdata
-    ./xmlchange DIN_LOC_ROOT_CLMFORC=/global/homes/c/cdkoven/scratch/inputdata/TRENDY2025/three_stream
+    #./xmlchange DIN_LOC_ROOT_CLMFORC=/global/homes/c/cdkoven/scratch/inputdata/TRENDY2025/three_stream
+    ./xmlchange DIN_LOC_ROOT_CLMFORC=/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7/TRENDY2025/three_stream
     #./xmlchange DATM_MODE=TRENDY2025
     
     cat > user_nl_elm <<EOF
@@ -108,33 +121,25 @@ elif [ "$STAGE" = "POSTAD_SPINUP" ]; then
     ./xmlchange RUN_STARTDATE=0001-01-01
     ./xmlchange RESUBMIT=0
     ./xmlchange ELM_ACCELERATED_SPINUP=off
-    ./xmlchange NTASKS=-5
+    ./xmlchange NTASKS=-8
     ./xmlchange STOP_N=1
     ./xmlchange REST_N=1
     ./xmlchange STOP_OPTION=nyears
     ./xmlchange JOB_QUEUE=debug
-    ./xmlchange CCSM_CO2_PPMV=287.
     ./xmlchange JOB_WALLCLOCK_TIME=00:30:00
     ./xmlchange -id ELM_BLDNML_OPTS -val "-bgc fates -no-megan -no-drydep"
 
-    # ./xmlchange RUN_TYPE=hybrid
-    # ./xmlchange RUN_REFCASE=fates_e3sm_perlmttr_fullmodel_4x5_test_landuse_nocomp_startfrompotentialveg_0006_bgcspinup_v1_377b2d31d7_ed007e30
-    # ./xmlchange RUN_REFDIR=/global/homes/c/cdkoven/scratch/e3sm_scratch/pm-cpu/fates_e3sm_perlmttr_fullmodel_4x5_test_landuse_nocomp_startfrompotentialveg_0006_bgcspinup_v1_377b2d31d7_ed007e30/run/
-    # #./xmlchange RUN_REFDIR=~/scratch/restfiles/
-    # ./xmlchange RUN_REFDATE=0261-01-01
-    # ./xmlchange GET_REFCASE=FALSE
-
-    #./xmlchange DATM_MODE=CLMCRUNCEP
+    ./xmlchange CCSM_CO2_PPMV=277.57
     ./xmlchange DATM_CLMNCEP_YR_START=1901
     ./xmlchange DATM_CLMNCEP_YR_END=1920
     ./xmlchange DATM_PRESAERO=clim_1850
 
-    ./xmlchange DIN_LOC_ROOT=/dvs_ro/cfs/cdirs/e3sm/inputdata
-    
+    ./xmlchange DIN_LOC_ROOT_CLMFORC=/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7/TRENDY2025/three_stream
+
     cat > user_nl_elm <<EOF
-fsurdat = '/global/cfs/cdirs/e3sm/inputdata/lnd/clm2/surfdata_map/surfdata_4x5_simyr2000_c130927.nc'
-flandusepftdat = '/global/homes/c/cdkoven/scratch/inputdata/fates_landuse_pft_map_4x5_20240206.nc'
-finidat='/global/homes/c/cdkoven/scratch/restfiles/f45_0029_adspinup_const1850LUH3_5fd137b0c9_13268f7a.elm.r.0286-01-01-00000.nc'
+fsurdat = '/dvs_ro/cfs/cdirs/e3sm/inputdata/lnd/clm2/surfdata_map/surfdata_1.9x2.5_simyr1850_c180306.nc'
+flandusepftdat = '/global/homes/c/cdkoven/scratch/inputdata/fates_landuse_pft_map_to_surfdata_1.9x2.5_250627.nc'
+finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0005_adspinup_const1700LUH_swdif_2pftspasturerangeland_39e91e09b5_7b982905.elm.r.0351-01-01-00000.nc'
 use_fates_luh = .true.
 use_fates_nocomp = .true.
 use_fates_fixed_biogeog = .true.
@@ -143,7 +148,7 @@ use_fates_sp = .false.
 fates_spitfire_mode = 1
 fates_harvest_mode = 'luhdata_area'
 use_fates_potentialveg = .false.
-fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH3_states_transitions_management.timeseries_4x5_hist_steadystate_1850_2025-05-22.nc'
+fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_1.9x2.5_trendy2025_steadystate_1700_2025-06-23.nc'
 use_century_decomp = .true.
 spinup_state = 0
 suplphos = 'ALL'
@@ -212,6 +217,6 @@ fi
 
 
 ./case.setup
-./preview_namelists
-#./case.build
-#./case.submit
+#./preview_namelists
+./case.build
+./case.submit

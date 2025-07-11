05be1527af086e6b9f014315bca0cda3f5e51dc1
Charles D Koven <cdkoven@lbl.gov>
2025-07-11 12:05:26 -0700

    refactored code to reduce duplicated text between cases

--- runscripts_trendy2025.bash
+++ runscripts_trendy2025.bash
@@ -26,17 +26,12 @@ cd $basedir
 export RES=f19_g16
 project=m2467
 
-#./create_newcase -case ${CASE_NAME} -res ${RES} -compset I1850GSWCNPRDCTCBCTOP -mach pm-cpu -project $project
 ./create_newcase -case ${CASE_NAME} -res ${RES} -compset I1850TRENDY2025 -mach pm-cpu -project $project
 
 
 cd $CASE_NAME
 
 ncgen -o fates_params_default_${GITHASH2}.nc ${SRCDIR}/external_models/fates/parameter_files/fates_params_default.cdl
-
-# reduce lifespan of grasses
-#/global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --pft 13 --var fates_mort_bmort --val 0.2 --O
-#/global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --pft 14 --var fates_mort_bmort --val 0.2 --O
 
 # Add additional 2 patches and pfts each for the pasture and rangeland
 /global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --O --var=fates_max_nocomp_pfts_by_landuse --val=4,4,2,2,1
@@ -48,46 +43,12 @@ ncgen -o fates_params_default_${GITHASH2}.nc ${SRCDIR}/external_models/fates/par
 # log daily
 /global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}.nc --O --var=fates_landuse_logging_event_code --val=3
 
-# debugging phase
-# ./xmlchange DEBUG=TRUE
-# ./xmlchange STOP_N=6
-# ./xmlchange STOP_OPTION=nmonths
-# ./xmlchange JOB_QUEUE=debug
+### changes for all cases.
+./xmlchange DIN_LOC_ROOT_CLMFORC=/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7/TRENDY2025/three_stream
+./xmlchange JOB_QUEUE=debug
+./xmlchange JOB_WALLCLOCK_TIME=00:30:00
 
-# fates_harvest_mode = 'no_harvest'
-# fates_harvest_mode = 'luhdata_area'
-
-if [ "$STAGE" = "AD_SPINUP"  ]; then
-
-    ./xmlchange RUN_STARTDATE=0001-01-01
-    #./xmlchange RUN_STARTDATE=0101-01-01  ## because taking a prior run's restart file in AD mode, so need to circumvent the 20 year thing
-    ./xmlchange RESUBMIT=3
-    ./xmlchange ELM_ACCELERATED_SPINUP=on
-    ./xmlchange -id ELM_BLDNML_OPTS -val "-bgc fates -no-megan -no-drydep -bgc_spinup on"
-    ./xmlchange NTASKS=-8
-    ./xmlchange STOP_N=50
-    ./xmlchange REST_N=5
-    ./xmlchange STOP_OPTION=nyears
-
-    ./xmlchange JOB_QUEUE=debug
-    ./xmlchange JOB_WALLCLOCK_TIME=00:30:00
-
-    # ./xmlchange JOB_QUEUE=regular
-    # ./xmlchange JOB_WALLCLOCK_TIME=20:00:00
-    
-    #./xmlchange DATM_MODE=CLMCRUNCEP
-
-    ./xmlchange CCSM_CO2_PPMV=277.57
-    ./xmlchange DATM_CLMNCEP_YR_START=1901
-    ./xmlchange DATM_CLMNCEP_YR_END=1920
-    ./xmlchange DATM_PRESAERO=clim_1850
-
-    #./xmlchange DIN_LOC_ROOT=/dvs_ro/cfs/cdirs/e3sm/inputdata
-    #./xmlchange DIN_LOC_ROOT_CLMFORC=/global/homes/c/cdkoven/scratch/inputdata/TRENDY2025/three_stream
-    ./xmlchange DIN_LOC_ROOT_CLMFORC=/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7/TRENDY2025/three_stream
-    #./xmlchange DATM_MODE=TRENDY2025
-    
-    cat > user_nl_elm <<EOF
+cat > user_nl_elm <<EOF
 fsurdat = '/dvs_ro/cfs/cdirs/e3sm/inputdata/lnd/clm2/surfdata_map/surfdata_1.9x2.5_simyr1850_c180306.nc'
 flandusepftdat = '/global/homes/c/cdkoven/scratch/inputdata/fates_landuse_pft_map_to_surfdata_1.9x2.5_250627.nc'
 use_fates_luh = .true.
@@ -98,9 +59,7 @@ use_fates_sp = .false.
 fates_spitfire_mode = 1
 fates_harvest_mode = 'luhdata_area'
 use_fates_potentialveg = .false.
-fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_1.9x2.5_trendy2025_steadystate_1700_2025-06-23.nc'
 use_century_decomp = .true.
-spinup_state = 1
 suplphos = 'ALL'
 suplnitro = 'ALL'
 fates_parteh_mode = 2
@@ -110,12 +69,28 @@ paramfile = '/global/homes/c/cdkoven/scratch/inputdata/clm_params_c211124_mod_dd
 fates_radiation_model = 'twostream'
 fates_leafresp_model = 'ryan1991'
 EOF
-## NOTE TO SWITCH RESP MODEL TO WORK WITH ROSIES PARAM FILE
-#fates_leafresp_model = 'atkin2017'
-#finidat = '/global/homes/c/cdkoven/scratch/restfiles/fates_4x5_nocomp_0009_bgcspinup_noseedrain_frombareground_const1850lu_6a011c67ac_cbfefff9.elm.r.0231-01-01-00000.nc'
-#fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_4x5_hist_steadystate_1850_21oct2024.nc'
-#fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_4x5_hist_steadystate_2000_2024-10-30.nc'
-#fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH3_states_transitions_management.timeseries_4x5_hist_steadystate_2000_2025-05-22.nc'
+
+if [ "$STAGE" = "AD_SPINUP"  ]; then
+
+    ./xmlchange RUN_STARTDATE=0001-01-01
+    ./xmlchange RESUBMIT=3
+    ./xmlchange ELM_ACCELERATED_SPINUP=on
+    ./xmlchange -id ELM_BLDNML_OPTS -val "-bgc fates -no-megan -no-drydep -bgc_spinup on"
+    ./xmlchange NTASKS=-8
+    ./xmlchange STOP_N=50
+    ./xmlchange REST_N=5
+    ./xmlchange STOP_OPTION=nyears
+
+    ./xmlchange CCSM_CO2_PPMV=277.57
+    ./xmlchange DATM_CLMNCEP_YR_START=1901
+    ./xmlchange DATM_CLMNCEP_YR_END=1920
+    ./xmlchange DATM_PRESAERO=clim_1850
+
+    cat >> user_nl_elm <<EOF
+fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_1.9x2.5_trendy2025_steadystate_1700_2025-06-23.nc'
+spinup_state = 1
+EOF
+
 elif [ "$STAGE" = "POSTAD_SPINUP" ]; then
 
     ./xmlchange RUN_STARTDATE=0001-01-01
@@ -125,8 +100,6 @@ elif [ "$STAGE" = "POSTAD_SPINUP" ]; then
     ./xmlchange STOP_N=1
     ./xmlchange REST_N=1
     ./xmlchange STOP_OPTION=nyears
-    ./xmlchange JOB_QUEUE=debug
-    ./xmlchange JOB_WALLCLOCK_TIME=00:30:00
     ./xmlchange -id ELM_BLDNML_OPTS -val "-bgc fates -no-megan -no-drydep"
 
     ./xmlchange CCSM_CO2_PPMV=277.57
@@ -134,37 +107,11 @@ elif [ "$STAGE" = "POSTAD_SPINUP" ]; then
     ./xmlchange DATM_CLMNCEP_YR_END=1920
     ./xmlchange DATM_PRESAERO=clim_1850
 
-    ./xmlchange DIN_LOC_ROOT_CLMFORC=/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7/TRENDY2025/three_stream
-
-    cat > user_nl_elm <<EOF
-fsurdat = '/dvs_ro/cfs/cdirs/e3sm/inputdata/lnd/clm2/surfdata_map/surfdata_1.9x2.5_simyr1850_c180306.nc'
-flandusepftdat = '/global/homes/c/cdkoven/scratch/inputdata/fates_landuse_pft_map_to_surfdata_1.9x2.5_250627.nc'
+    cat >> user_nl_elm <<EOF
 finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0005_adspinup_const1700LUH_swdif_2pftspasturerangeland_39e91e09b5_7b982905.elm.r.0351-01-01-00000.nc'
-use_fates_luh = .true.
-use_fates_nocomp = .true.
-use_fates_fixed_biogeog = .true.
-fates_paramfile = '${basedir}/${CASE_NAME}/fates_params_default_${GITHASH2}.nc'
-use_fates_sp = .false.
-fates_spitfire_mode = 1
-fates_harvest_mode = 'luhdata_area'
-use_fates_potentialveg = .false.
 fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_1.9x2.5_trendy2025_steadystate_1700_2025-06-23.nc'
-use_century_decomp = .true.
 spinup_state = 0
-suplphos = 'ALL'
-suplnitro = 'ALL'
-fates_parteh_mode = 2
-nu_com = 'RD'
-hist_fincl1 = 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU','PROD10C','PROD100C','PRODUCT_CLOSS'
-paramfile = '/global/homes/c/cdkoven/scratch/inputdata/clm_params_c211124_mod_ddefold.nc'
-fates_radiation_model = 'twostream'
-fates_leafresp_model = 'ryan1991'
 EOF
-
-# make the seed rain amount nonzero for grasses (c4 and cool c3 only)
-# /global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}.nc --fout=fates_params_default_${GITHASH2}_mod.nc --pft 12 --var fates_recruit_seed_supplement --val 1e-3
-# /global/homes/c/cdkoven/E3SM/components/elm/src/external_models/fates/tools/modify_fates_paramfile.py --fin=fates_params_default_${GITHASH2}_mod.nc --fout=fates_params_default_${GITHASH2}_mod.nc --pft 11 --var fates_recruit_seed_supplement --val 1e-3 --O
-
 
 elif [ "$STAGE" = "TRANSIENT_LU_CONSTANT_CO2_CLIMATE" ]; then
 

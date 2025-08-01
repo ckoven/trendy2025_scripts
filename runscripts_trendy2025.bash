35eb61d2048391d39e5796b78f5085ae0a28367d
Charles D Koven <cdkoven@lbl.gov>
2025-08-01 11:33:29 -0700

    added notebooks and updates runscript

--- runscripts_trendy2025.bash
+++ runscripts_trendy2025.bash
@@ -9,9 +9,9 @@ GITHASH2=`git log -n 1 --format=%h`
 #STAGE=AD_SPINUP
 #STAGE=POSTAD_SPINUP
 #STAGE=S0   #constant climate, CO2, land use
-STAGE=S1   #CO2 only (time-invariant “pre-industrial” climate and land use mask)
+#STAGE=S1   #CO2 only (time-invariant “pre-industrial” climate and land use mask)
 #STAGE=S2   #CO2 and climate only (time-invariant “pre-industrial” land use mask), branch from S1 in year 1901
-#STAGE=S3a  #CO2 and land use (repeat climate until year 1901)
+STAGE=S3a  #CO2 and land use (repeat climate until year 1901)
 #STAGE=S3b  #CO2, climate and land use (all forcing time-varying after year 1901)
 
 if [ "$STAGE" = "AD_SPINUP" ]; then
@@ -65,10 +65,10 @@ ncgen -o fates_params_default_${GITHASH2}.nc ${SRCDIR}/external_models/fates/par
 ./xmlchange DIN_LOC_ROOT_CLMFORC=/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7/TRENDY2025/three_stream
 ./xmlchange DIN_LOC_ROOT=/dvs_ro/cfs/cdirs/e3sm/inputdata
 ./xmlchange JOB_QUEUE=debug
-./xmlchange JOB_WALLCLOCK_TIME=00:30:00
+./xmlchange JOB_WALLCLOCK_TIME=00:15:00
 ./xmlchange NTASKS=-8
 ./xmlchange STOP_N=50
-./xmlchange REST_N=5
+./xmlchange REST_N=10
 ./xmlchange STOP_OPTION=nyears
 ./xmlchange RESUBMIT=3
 
@@ -89,7 +89,7 @@ suplphos = 'ALL'
 suplnitro = 'ALL'
 fates_parteh_mode = 2
 nu_com = 'RD'
-hist_fincl1 = 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU','PROD10C','PROD100C','PRODUCT_CLOSS','FATES_SEEDS_IN_PF','FATES_SEEDS_IN_LOCAL_PF','FATES_NPLANT_CANOPY_SZPF','FATES_NPLANT_USTORY_SZPF','FATES_MORTALITY_USTORY_SZPF','FATES_MORTALITY_CANOPY_SZPF','FATES_CWD_ABOVEGROUND_DC','FATES_CWD_BELOWGROUND_DC','FATES_LEAF_ALLOC_SZPF','FATES_SEED_ALLOC_SZPF','FATES_FROOT_ALLOC_SZPF','FATES_BGSAPWOOD_ALLOC_SZPF','FATES_BGSTRUCT_ALLOC_SZPF','FATES_AGSAPWOOD_ALLOC_SZPF','FATES_AGSTRUCT_ALLOC_SZPF','FATES_STORE_ALLOC_SZPF','FATES_DDBH_CANOPY_SZPF','FATES_DDBH_USTORY_SZPF','FATES_NPLANT_ACPF'
+hist_fincl1 = 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU','PROD10C','PROD100C','PRODUCT_CLOSS','FATES_SEEDS_IN_PF','FATES_SEEDS_IN_LOCAL_PF','FATES_NPLANT_CANOPY_SZPF','FATES_NPLANT_USTORY_SZPF','FATES_MORTALITY_USTORY_SZPF','FATES_MORTALITY_CANOPY_SZPF','FATES_CWD_ABOVEGROUND_DC','FATES_CWD_BELOWGROUND_DC','FATES_LEAF_ALLOC_SZPF','FATES_SEED_ALLOC_SZPF','FATES_FROOT_ALLOC_SZPF','FATES_BGSAPWOOD_ALLOC_SZPF','FATES_BGSTRUCT_ALLOC_SZPF','FATES_AGSAPWOOD_ALLOC_SZPF','FATES_AGSTRUCT_ALLOC_SZPF','FATES_STORE_ALLOC_SZPF','FATES_DDBH_CANOPY_SZPF','FATES_DDBH_USTORY_SZPF','FATES_NPLANT_ACPF','FATES_LAI_USTORY_SZPF','FATES_LAI_CANOPY_SZPF'
 paramfile = '/global/homes/c/cdkoven/scratch/inputdata/clm_params_c211124_mod_ddefold.nc'
 fates_radiation_model = 'twostream'
 fates_leafresp_model = 'ryan1991'
@@ -131,12 +131,13 @@ elif [ "$STAGE" = "S0" ]; then
     ./xmlchange RUN_STARTDATE=1701-01-01
     ./xmlchange ELM_ACCELERATED_SPINUP=off
     ./xmlchange -id ELM_BLDNML_OPTS -val "-bgc fates -no-megan -no-drydep"
+    ./xmlchange CCSM_CO2_PPMV=277.57
     ./xmlchange DATM_CLMNCEP_YR_START=1901
     ./xmlchange DATM_CLMNCEP_YR_END=1920
     ./xmlchange DATM_PRESAERO=clim_1850
 
     cat >> user_nl_elm <<EOF
-finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0005_adspinup_const1700LUH_swdif_2pftspasturerangeland_39e91e09b5_7b982905.elm.r.0351-01-01-00000.nc'
+finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0006_postadspinup_const1700LUH_39e91e09b5_7b982905.elm.r.0381-01-01-00000.nc'
 fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_1.9x2.5_trendy2025_steadystate_1700_2025-06-23.nc'
 spinup_state = 0
 EOF
@@ -153,7 +154,7 @@ elif [ "$STAGE" = "S1" ]; then
     ./xmlchange DATM_PRESAERO=clim_1850
     
     cat >> user_nl_elm <<EOF
-finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0005_adspinup_const1700LUH_swdif_2pftspasturerangeland_39e91e09b5_7b982905.elm.r.0351-01-01-00000.nc'
+finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0006_postadspinup_const1700LUH_39e91e09b5_7b982905.elm.r.0381-01-01-00000.nc'
 fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/LUH2_states_transitions_management.timeseries_1.9x2.5_trendy2025_steadystate_1700_2025-06-23.nc'
 EOF
 
@@ -169,7 +170,7 @@ elif [ "$STAGE" = "S3a" ]; then
     ./xmlchange DATM_PRESAERO=clim_1850
     
     cat >> user_nl_elm <<EOF
-finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0005_adspinup_const1700LUH_swdif_2pftspasturerangeland_39e91e09b5_7b982905.elm.r.0351-01-01-00000.nc'
+finidat='/global/homes/c/cdkoven/scratch/restfiles/f19_0006_postadspinup_const1700LUH_39e91e09b5_7b982905.elm.r.0381-01-01-00000.nc'
 fluh_timeseries = '/global/homes/c/cdkoven/scratch/inputdata/luh2_trendy2025_1.9x2.5_850-2024_23jun2025.nc'
 EOF
 
@@ -191,4 +192,9 @@ if [ "$COMPSET" = "I20TRTRENDY2025"  ]; then
 fi
 
 ./case.build
+./case.submit
+
+#####
+./xmlchange JOB_QUEUE=regular
+./xmlchange JOB_WALLCLOCK_TIME=17:00:00
 ./case.submit
